0e4419fc3152de7eae70c390276e1dedb4319bd3
Антон Ивашкин <aivash.tech@gmail.com>
2026-02-05 01:27:03 +0300

    feat(expense): add index request validation for filtering and pagination

--- app/Http/Controllers/Expense/IndexController.php
+++ app/Http/Controllers/Expense/IndexController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Expense;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Expense\IndexRequest;
 use App\Http\Resources\ExpenseResource;
 use App\QueryBuilders\ExpenseQuery;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(IndexRequest $request)
     {
         $perPage = $request->integer('per_page', 10);
 
---
+++ app/Http/Requests/Expense/IndexRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Expense;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'from' => ['nullable', 'date'],
+            'to' => ['nullable', 'date', 'after_or_equal:from'],
+
+            'sort' => [
+                'nullable',
+                Rule::in(['spent_at', 'amount', 'created_at']),
+            ],
+
+            'direction' => [
+                'nullable',
+                Rule::in(['asc', 'desc']),
+            ],
+
+            'per_page' => [
+                'nullable',
+                'integer',
+                'min:1',
+                'max:100',
+            ],
+        ];
+    }
+}
